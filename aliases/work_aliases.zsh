ea01960c23f72778d2d5756169fd7629e4087d42
Joe Williams <joe.williams1@kraken.tech>
2025-06-16 12:46:28 +0100

    Update work_aliases

--- aliases/work_aliases.zsh
+++ aliases/work_aliases.zsh
@@ -2,15 +2,13 @@
 #   Work Aliases
 #   -------------------------------
 
-alias fj='cd ~/Development/Flapjack'
-alias fj2='cd ~/Development/Flapjack2'
+alias flapjack='cd ~/SourceCode/Flapjack'
 alias generate-tuist='source "$SCRIPTS_DIR/lib.sh" && close_xcode_if_running && tuist generate --path iOS'
-alias push-no-checks='git push --no-verify'
 alias run-shared-tests='./gradlew allTests'
 alias run-ios-tests='./gradlew iosSimulatorArm64Test'
 alias run-android-ui-tests='./gradlew :androidApp:connectedOctopusDebugAndroidTest'
 alias run-spotless='./gradlew spotlessApply'
-alias symmetry='cd ~/Development/Flapjack/iOS/Packages/Symmetry'
+alias symmetry='cd ~/SourceCode/Flapjack/iOS/Packages/Symmetry'
 alias local-properties='open local.properties'
 
 #   -------------------------------
